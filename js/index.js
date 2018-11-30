fbb55ba1bf1d2581ec3aa521f1df57dfe3400a8a
Kabir Shah <iamkbrsh@gmail.com>
2018-11-30 04:01:42 +0000

    add minigame

--- js/index.js
+++ js/index.js
@@ -19,3 +19,39 @@ function detectNavLight() {
 // Check at the beginning and when the user scrolls
 detectNavLight();
 window.addEventListener("scroll", detectNavLight);
+
+// Hidden secret
+var secret = false;
+window.addEventListener("click", function(event) {
+    if (!secret && event.detail === 3) {
+        secret = true;
+        
+        document.body.innerHTML = `<div class="secret"><canvas></canvas><iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
+        
+        var canvas = document.querySelector(".secret canvas");
+        var context = canvas.getContext("2d");
+        
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        
+        var previousX, previousY;
+        
+        function placeDot(event) {
+            if (!previousX) {
+                previousX = event.offsetX;
+                previousY = event.offsetY;
+            }
+            
+            context.beginPath();
+            context.moveTo(previousX, previousY);
+            context.lineTo(event.offsetX, event.offsetY);
+            context.stroke();
+            
+            previousX = event.offsetX;
+            previousY = event.offsetY;
+        }
+        
+        canvas.addEventListener("mousedown", placeDot);
+        canvas.addEventListener("mousemove", placeDot);
+    }
+});
